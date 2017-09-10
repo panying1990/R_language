369702856e3778b2e646612e213a5c0c0f96c61a
panying <panying1298247841@163.com>
2017-09-10 17:08:20 +0800

    Update k_mean.R

--- k_mean.R
+++ k_mean.R
@@ -35,5 +35,45 @@
   table(teens$female, useNA = "ifany")
   table(teens$no_gender, useNA = "ifany")
   #1.2 插补法
+  # 寻找字段分布状态，以age为例
+  mean(teens$age) # 因为有NA值，故无法进行
+  mean(teens$age, na.rm = TRUE) 
+  
+  # 利用毕业时间计算年龄
+  aggregate(data = teens, age ~ gradyear, mean, na.rm = TRUE)
+  # 为每个实例计算年龄
+  ave_age <- ave(teens$age, teens$gradyear,FUN = function(x) mean(x, na.rm = TRUE))
+  teens$age <- ifelse(is.na(teens$age), ave_age, teens$age)
+  # 检查插补后的数据，无NA值
+  summary(teens$age)
   #2、极端值处理，将不符合基本逻辑的极端值设置为NA
   teens$age <- ifelse(teens$age >= 13 & teens$age < 20,teens$age, NA)
+  
+  ## 第三步: 基于数据训练模型 ----
+  interests <- teens[5:40]
+  interests_z <- as.data.frame(lapply(interests, scale))
+  
+  teen_clusters <- kmeans(interests_z, 5)
+  
+  ## Step 4: Evaluating model performance ----
+  # look at the size of the clusters
+  teen_clusters$size
+  
+  # look at the cluster centers
+  teen_clusters$centers
+  
+  ## Step 5: Improving model performance ----
+  # apply the cluster IDs to the original data frame
+  teens$cluster <- teen_clusters$cluster
+  
+  # look at the first five records
+  teens[1:5, c("cluster", "gender", "age", "friends")]
+  
+  # mean age by cluster
+  aggregate(data = teens, age ~ cluster, mean)
+  
+  # proportion of females by cluster
+  aggregate(data = teens, female ~ cluster, mean)
+  
+  # mean number of friends by cluster
+  aggregate(data = teens, friends ~ cluster, mean)
